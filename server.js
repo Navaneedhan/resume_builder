eb21c6d2beb7e0c0d0e37564e31b4587eed14361
navaneedhan <navaneedhan@chronuscorp.com>
2018-06-22 22:35:46 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,16 @@
+// Including Express
+const express = require('express');
+const app = express();
+
+// Connecting with the port
+app.listen(3000, function() {
+	console.log('listening to the port 3000');
+});
+
+app.get('/', function(req, res) {
+	res.send('Hello space');
+});
+
+app.get('/about_us', function(req, res) {
+	res.sendFile(__dirname + '/about_us.html');
+});
